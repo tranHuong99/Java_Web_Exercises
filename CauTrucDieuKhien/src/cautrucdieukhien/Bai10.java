e73ecc3a09beaccda8894c05f97b2cdc6963052c
tranHuong99 <tranhuong.hvact@gmail.com>
2019-01-19 00:03:01 +0700

    Thong bao moi

---
+++ CauTrucDieuKhien/src/cautrucdieukhien/Bai10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cautrucdieukhien;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HUONG XINH GAI
+ */
+public class Bai10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập n = ");
+        int n = sc.nextInt();
+        
+    }
+}
