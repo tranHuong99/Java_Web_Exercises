549717bbd29777cc71b18396eae10d77a55f6a8b
tranHuong99 <tranhuong.hvact@gmail.com>
2019-01-21 12:17:23 +0700

    Thong bao

--- CauTrucDieuKhien/src/cautrucdieukhien/Bai11_1.java
+++ CauTrucDieuKhien/src/cautrucdieukhien/Bai11_1.java
@@ -5,29 +5,71 @@
  */
 package cautrucdieukhien;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HUONG XINH GAI
  */
 public class Bai11_1 {
-    public static int tachSo(int n){
+    public static void tachSo(int n){
         int temp, count=0;
         String str = new String();
         while(n!=0){
             temp = n%10;
             n /= 10;
             count++;
-        }
-        switch(count){
-            case 1: str = "một, hai, ba, bốn, năm, sáu, bảy, tám, chín";
-                break;
-            case 2: str = "mươi";
-                break;
-            case 3: str = "trăm";
-                break;
+            switch(count){
+                case 1: System.out.print("hang don vi");
+                    break;
+                case 2: System.out.print("hang chuc");
+                    break;
+                case 3: System.out.print("hang tram");
+                    break;
+            }
         }
     }
+    public static String chuyenDoiGiaTri(int n){
+        String str = new String();
+        switch(n){
+            case 0: str = "không";
+                break;
+            case 1: str = "một";
+                break;
+            case 2: str = "hai";
+                break;
+            case 3: str = "ba";
+                break;
+            case 4: str = "bốn";
+                break;
+            case 5: str = "năm";
+                break;
+            case 6: str = "sáu";
+                break;
+            case 7: str = "bảy";
+                break;
+            case 8: str = "tám";
+                break;
+            case 9: str = "chín";
+        }
+        return str;
+    }
+    public static String chuyenDoiHang(int count){
+        String str = new String();
+        switch(count){
+            case 0: break;
+            case 1: str = chuyenDoiGiaTri(count);
+            case 2: str = "mươi";
+            case 3: str = "trăm";
+        }
+        return str;
+    }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập n = ");
+        int n = sc.nextInt();
+        String str = new String();
+        System.out.printf("Số %d đọc là %s %s", n, chuyenDoiGiaTri(str), chuyenDoiHang(str));
         
     }
 }
